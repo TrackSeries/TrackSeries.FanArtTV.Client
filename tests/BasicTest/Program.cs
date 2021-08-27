75bd202ab9eea9ccb27486163d50d758ab8258b4
Carlos J. Aliaga <carlos@cjaliaga.net>
2021-08-27 18:08:23 +0200

    Movies basic calls

--- tests/BasicTest/Program.cs
+++ tests/BasicTest/Program.cs
@@ -19,8 +19,12 @@ var provider = services.BuildServiceProvider();
 
 var client = provider.GetRequiredService<IFanArtTVClient>();
 
+// Shows
 var gameOfThrones = await client.TV.GetShowAsync(121361, default);
+var latestShows = await client.TV.GetLatestShowsAsync(DateTime.Now.AddDays(-7), default);
 
-var latest = await client.TV.GetLatestShowsAsync(DateTime.Now.AddDays(-7), default);
+// Movies
+var lordOfTheRings = await client.Movies.GetMovieAsync("120", default);
+var latestMovies = await client.Movies.GetLatestMoviesAsync(DateTime.Now.AddDays(-7), default);
 
 Console.WriteLine("Popcorn");
